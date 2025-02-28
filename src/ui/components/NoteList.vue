d3fdb3cae1003f1325b380adee32edc4abdca66e
Adrians <ak00311@gmail.com>
2025-02-28 10:45:10 +0200

    notes

---
+++ src/ui/components/NoteList.vue
@@ -0,0 +1,100 @@
+<script setup>
+import SideTaskbarCard from '../components/SideTaskbarCard.vue'
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="hotbar">
+      <input class="searchbar" placeholder="search...">
+      <button>smth.</button>
+      <button>filter by ...</button>
+      <button>Edit tasks</button>
+      <!--    when edit tasks will be enabled, then remove card hover transform -->
+      <button>-></button>
+    </div>
+    <div class="taskbar">
+      <SideTaskbarCard title="title" time="23.23.3425"/>
+      <SideTaskbarCard title="title2" time="23.23.33`15"/>
+      <SideTaskbarCard title="213312b" time="23.23.123425"/>
+      <SideTaskbarCard title="title is bigger" time="23.23.3425"/>
+      <SideTaskbarCard title="title IS EVEN BIGGER THAN BEFORE" time="23.23.3425"/>
+      <SideTaskbarCard title="213312b" time="23.23.123425"/>
+      <SideTaskbarCard title="title is bigger" time="23.23.3425"/>
+      <SideTaskbarCard title="title IS EVEN BIGGER THAN BEFORE" time="23.23.3425"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.hotbar {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  flex: 0.5;
+
+  padding-left: 2px;
+  padding-right: 2px;
+  box-sizing: border-box;
+
+
+  background: #979797;
+}
+.hotbar button{
+  height: 100%;
+  min-width: 3em;
+  background: transparent;
+  border: none;
+}
+button:hover {
+  backdrop-filter: brightness(0.8);
+}
+.searchbar {
+  width: 100%;
+  height: 70%;
+  border-radius: 2px;
+  border: none;
+}
+
+.taskbar {
+  flex: 10;
+
+  display: grid;
+  grid-auto-rows: 9em;
+  grid-row-gap: 3px;
+  padding: 2px;
+
+  box-sizing: border-box;
+  overflow: scroll;
+}
+
+
+.wrapper{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: calc(100% - 20px);
+  width: calc(20vw - 20px);
+  margin: 10px;
+
+  padding: 2px;
+  box-sizing: border-box;
+  border-radius: var(--inner-border);
+
+  z-index: 0;
+  background: #7a7a7a;
+}
+.wrapper::before{
+  content: '';
+  position: absolute;
+  border-radius: calc(var(--inner-border) - 2px);
+  height: calc(100% - var(--inner-border) * 2);
+  width: calc(100% - var(--inner-border) * 2);
+  filter: brightness(.8);
+  background: gray;
+  z-index: -1;
+}
+
+</style>
--- src/ui/components/cards/ApiaryCard.vue
+++ src/ui/components/cards/ApiaryCard.vue
@@ -1,14 +1,16 @@
 <script setup>
+import NoteList from "@/ui/components/NoteList.vue";
+
 defineProps({
   name: String
 })
 </script>
 
 <template>
-  <div class="item-card-wrapper display-flex-row">
+  <div class="item-card-wrapper display-flex-row cut-rounded-border">
     <div class="display-flex-column flex-2">
 
-      <div class="top-row">
+      <div class="top-row cut-rounded-border">
         <h1>NR. 1</h1>
         <h2 class="flex-1">NAme n shit</h2>
         <div>options</div>
@@ -18,20 +20,20 @@ defineProps({
       <div class="bottom-row flex-1 display-flex-row">
 
         <div class="first-column">
-          <div class="image-container"><img class="this-card-image" src="/src/ui/assets/images/honeyCombWall.jpg" alt=""></div>
+          <div class="image-container cut-rounded-border"><img class="this-card-image" src="/src/ui/assets/images/honeyCombWall.jpg" alt=""></div>
           <hr class="hr-horizontal-transparent">
-          <div class="description">description</div>
+          <div class="description cut-rounded-border">description</div>
         </div>
 
         <hr class="hr-vertical">
 
         <div class="second-column">
-          <div class="notes">notes</div>
+         <NoteList></NoteList>
         </div>
 
         <hr class="hr-vertical">
 
-        <div class="third-column">
+        <div class="third-column cut-rounded-border">
           <div class="hives">hives</div>
           <hr class="hr-horizontal-transparent">
           <div class="weather">weather</div>
@@ -42,12 +44,13 @@ defineProps({
       </div>
     </div>
     <hr class="hr-vertical">
-    <div class="history">Action history</div>
+    <div class="history cut-rounded-border">Action history</div>
   </div>
 </template>
 
 <style scoped>
 .item-card-wrapper {
+  aspect-ratio: 10/4;
   width: 100%;
   background: #ac4d0a;
   transition: .1s;
@@ -55,7 +58,8 @@ defineProps({
 }
 .top-row {
   display: flex;
-  height: 20%;
+  backdrop-filter: blur(3px);
+  height: 15%;
   background: var(--transparent-white-80);
 }
 .first-column {
