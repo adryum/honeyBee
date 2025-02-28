931a4ff21012760a67aabcd1aa00154e8ed945e9
Adrians <ak00311@gmail.com>
2025-02-28 09:47:52 +0200

    apiary

--- src/ui/components/cards/ApiaryCard.vue
+++ src/ui/components/cards/ApiaryCard.vue
@@ -5,61 +5,110 @@ defineProps({
 </script>
 
 <template>
-  <div class="item-card-wrapper cut-rounded-border display-flex-row">
+  <div class="item-card-wrapper display-flex-row">
     <div class="display-flex-column flex-2">
-      <div class="top-row bg-white display-flex-row">
+
+      <div class="top-row">
         <h1>NR. 1</h1>
         <h2 class="flex-1">NAme n shit</h2>
         <div>options</div>
       </div>
+
       <hr class="hr-horizontal-transparent">
-      <div class="bottom-row display-flex-row flex-1">
-        <div class="left-side flex-3 display-flex-column">
-          <div class="image-container bg-white"><img class="card-image" src="/src/ui/assets/images/honeyCombWall.jpg" alt=""></div>
+      <div class="bottom-row flex-1 display-flex-row">
 
+        <div class="first-column">
+          <div class="image-container"><img class="this-card-image" src="/src/ui/assets/images/honeyCombWall.jpg" alt=""></div>
           <hr class="hr-horizontal-transparent">
-
-          <div class="bottom flex-1 display-flex-row">
-            <div class="description flex-1 bg-white">description</div>
-            <hr class="hr-vertical">
-            <div class="notes flex-1 bg-white">notes</div>
-          </div>
+          <div class="description">description</div>
         </div>
 
         <hr class="hr-vertical">
 
-        <div class="right-side display-flex-column flex-2">
-          <div class="flex-1 bg-white">hives</div>
-          <hr class="hr-horizontal-transparent">
-          <div class="flex-1 bg-white">weather</div>
-          <hr class="hr-horizontal-transparent">
-          <div class="flex-1 bg-white">location</div>
+        <div class="second-column">
+          <div class="notes">notes</div>
         </div>
+
+        <hr class="hr-vertical">
+
+        <div class="third-column">
+          <div class="hives">hives</div>
+          <hr class="hr-horizontal-transparent">
+          <div class="weather">weather</div>
+          <hr class="hr-horizontal-transparent">
+          <div class="location">location</div>
+        </div>
+
       </div>
     </div>
     <hr class="hr-vertical">
-    <div class="flex-1 bg-white">Action history</div>
+    <div class="history">Action history</div>
   </div>
 </template>
 
 <style scoped>
 .item-card-wrapper {
   width: 100%;
-  height: 25vw;
-  font-size: 1rem;
-
+  background: #ac4d0a;
   transition: .1s;
+  padding: 3px;
 }
-.left-side {
-  align-items: stretch;
+.top-row {
+  display: flex;
+  height: 20%;
+  background: var(--transparent-white-80);
 }
+.first-column {
+  display: flex;
+  flex-direction: column;
+  flex: 1 2 30%;
+}
+.second-column {
+  display: flex;
+  flex-direction: column;
+  flex: 30%;
+}
+.third-column {
+  display: flex;
+  flex-direction: column;
+  flex: 1 2 20%;
+}
+.third-column > div {
+  flex: auto;
+}
+.hives{
+  background: var(--transparent-white-80);
+}
+.weather {
+  background: var(--transparent-white-80);
+}
+.location {
+  background: var(--transparent-white-80);
+}
+.history {
+  flex: 1;
+  background: var(--transparent-white-80);
+}
+.notes {
+  flex: 1 1 50%;
+  background: var(--transparent-white-80);
+}
+.description {
+  flex: 1 50%;
+  background: var(--transparent-white-80);
+}
+.right-side > div {
+  flex: 1;
+}
+
 .image-container {
-
-
+  max-height: 50%;
+  background: var(--transparent-white-80);
 }
-.card-image {
+.this-card-image {
   width: 100%;
   height: 100%;
+  object-position: center;
   object-fit: cover;
 }
 </style>
